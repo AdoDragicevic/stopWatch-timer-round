55e9fee232a0d0c3283c9540b2901b9ec1e3c658
Ado Dragicevic <adodragicevic@gmail.com>
2021-04-26 01:14:00 +0200

    Delete files moved to folders from the root dir

--- app.js
+++
@@ -1,7 +0,0 @@
-//SELECTORS
-const inputs    = document.querySelectorAll(".input"),
-      buttons   = document.querySelectorAll("#reset, #start"),
-      switchBtn = document.querySelector("#switch");
-
-//SETUP
-const apps = new SwitchBetweenApps( [ new Timer(), new Round(), new StopWatch(true) ] );
