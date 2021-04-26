a2275940da0f98ff365671b541385e15f00788a8
Ado Dragicevic <adodragicevic@gmail.com>
2021-04-26 21:28:41 +0200

    Add files via upload

--- js/DOMManipulation/styleTimer.js
+++ js/DOMManipulation/styleTimer.js
@@ -7,14 +7,26 @@ class StyleTimer extends Style {
         super();
 
         this.runOnUpdateDisplay.push(
-            this.updateInputLabelTxt.bind(this, ["hours", "seconds", "ms"])
+            this.updateInputLabelTxt.bind(this, ["hours", "minutes", "seconds"])
         );
+
+        this.runOnRunTime.push(
+            this.runLine
+        );
+
     }
 
 
     updateBtnTxt() {
         document.querySelector("#reset").innerText = "Reset";
         document.querySelector("#start").innerText = this.isRunning ? "Stop" : "Start";
+    }
+
+
+    runLine() {
+        let line = document.querySelector(".line");
+        console.log(line);
+        line.value = "12"
     }
 
 
